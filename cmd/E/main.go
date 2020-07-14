3fc11b70b2d10c3a093200b432d3426000476865
Andreas Tolfsen <ato@sny.no>
2020-07-14 14:12:50 +0200

    cmd/E, edit: move RE to package

--- cmd/E/main.go
+++ cmd/E/main.go
@@ -4,23 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net/rpc"
 	"os"
-	"path/filepath"
-	"strings"
 
 	"sny.no/tools/edit"
 )
 
-const (
-	EX_USAGE  = 64
-	EX_NOHOST = 68
-)
-
-var (
-	sshconn = env("SSH_CONNECTION", "")
-	edconn  = env("EDITOR_CONNECTION", "localhost:52670")
-)
+const EX_USAGE  = 64
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "usage: %s [+line] file...\n", os.Args[0])
@@ -37,11 +26,8 @@ func main() {
 }
 
 func E(arg ...string) {
-	if sshconn != "" {
-		if len(edconn) < 0 {
-			log.Fatal("missing EDITOR_CONNECTION")
-		}
-		ex, err := RE(arg...)
+	if os.Getenv("SSH_CONNECTION") != "" {
+		ex, err := edit.RE(arg...)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -51,47 +37,4 @@ func E(arg ...string) {
 			log.Fatal(err)
 		}
 	}
-}
-
-func RE(arg ...string) (ex edit.ExitCode, err error) {
-	client, err := rpc.Dial("tcp", edconn)
-	if err != nil {
-		return EX_NOHOST, err
-	}
-	req := edit.Request{
-		Args: realpaths(arg),
-		//Environ: []string{fmt.Sprintf("NAMESPACE=%s", ns)},
-	}
-	err = client.Call("Edit.E", req, &ex)
-	return ex, err
-}
-
-// Normalizes the paths of existing files,
-// assumes non-existing files are in CWD,
-// and skips flags that start with "-".
-func realpaths(args []string) (normargs []string) {
-	for _, arg := range args {
-		if strings.Index(arg, "-") == 0 {
-			normargs = append(normargs, arg)
-		} else if _, err := os.Stat(arg); err == nil {
-			path, _ := filepath.Abs(arg)
-			normargs = append(normargs, path)
-		} else if os.IsNotExist(err) {
-			cwd, _ := os.Getwd()
-			path := filepath.Join(cwd, arg)
-			normargs = append(normargs, path)
-		} else {
-			// Schrödinger: file may or may not exist
-			log.Fatal(err)
-		}
-	}
-	return normargs
-}
-
-func env(key, fallbackv string) string {
-	v, ok := os.LookupEnv(key)
-	if !ok {
-		return fallbackv
-	}
-	return v
 }
